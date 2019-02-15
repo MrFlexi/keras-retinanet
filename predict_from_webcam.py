2c6c3d0f1ee6c5d4af4e0b59467f300d9535f023
MrFlexi <Szaroletta@hotmail.com>
2019-02-15 11:37:44 +0100

    New crop aerea when recording from webcam

--- predict_from_webcam.py
+++ predict_from_webcam.py
@@ -51,21 +51,10 @@ model_path = os.path.join('snapshots', 'resnet50_lego_01.h5')
 # load retinanet model
 model = models.load_model(model_path, backbone_name='resnet50')
 
-# if the model is not converted to an inference model, use the line below
-# see: https://github.com/fizyr/keras-retinanet#converting-a-training-model-to-inference-model
-#model = models.convert_model(model)
-
-#print(model.summary())
-
-# load label to names mapping for visualization purposes
-#labels_to_names = {0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 4: 'airplane', 5: 'bus', 6: 'train', 7: 'truck', 8: 'boat', 9: 'traffic light', 10: 'fire hydrant', 11: 'stop sign', 12: 'parking meter', 13: 'bench', 14: 'bird', 15: 'cat', 16: 'dog', 17: 'horse', 18: 'sheep', 19: 'cow', 20: 'elephant', 21: 'bear', 22: 'zebra', 23: 'giraffe', 24: 'backpack', 25: 'umbrella', 26: 'handbag', 27: 'tie', 28: 'suitcase', 29: 'frisbee', 30: 'skis', 31: 'snowboard', 32: 'sports ball', 33: 'kite', 34: 'baseball bat', 35: 'baseball glove', 36: 'skateboard', 37: 'surfboard', 38: 'tennis racket', 39: 'bottle', 40: 'wine glass', 41: 'cup', 42: 'fork', 43: 'knife', 44: 'spoon', 45: 'bowl', 46: 'banana', 47: 'apple', 48: 'sandwich', 49: 'orange', 50: 'broccoli', 51: 'carrot', 52: 'hot dog', 53: 'pizza', 54: 'donut', 55: 'cake', 56: 'chair', 57: 'couch', 58: 'potted plant', 59: 'bed', 60: 'dining table', 61: 'toilet', 62: 'tv', 63: 'laptop', 64: 'mouse', 65: 'remote', 66: 'keyboard', 67: 'cell phone', 68: 'microwave', 69: 'oven', 70: 'toaster', 71: 'sink', 72: 'refrigerator', 73: 'book', 74: 'clock', 75: 'vase', 76: 'scissors', 77: 'teddy bear', 78: 'hair drier', 79: 'toothbrush'}
-#labels_to_names = {0: 'person', 1: 'PinBlack', 2: '1x15Blue', 3: '1x9Red', 4: 'JunctionGray', 5: '1x7Red', 6: '1x13Gray', 7: '1x4LRed'}
-
-
 def predict( image ):
     # copy to draw on
     draw = image.copy()
-    draw = cv2.cvtColor(draw, cv2.COLOR_BGR2RGB)
+    # draw = cv2.cvtColor(draw, cv2.COLOR_BGR2RGB)
 
     # preprocess image for network
     image = preprocess_image(image)
@@ -95,16 +84,30 @@ def predict( image ):
         caption = "{} {:.3f}".format(labels_to_names[label], score)
         draw_caption(draw, b, caption)
 
-    plt.figure(figsize=(15, 15))
-    plt.axis('off')
-    plt.imshow(draw)
-    plt.show()
+    cv2.imshow('Prediction', draw)
+
+def webcam( ):
+    picturePath = './webcam/images/frame4.jpg'
+    imageA = cv2.imread('./webcam/images/background.jpg')
+
+    cap = cv2.VideoCapture(0)
+    count = 0
+    while (True):
+
+        ret, frame = cap.read()
+        croppend_image = frame[100:350, 200:450].copy()
+        cv2.rectangle(frame, (200, 100), (450, 350), (0, 255, 255), 2)
+        cv2.imshow('WebCam', frame)
+        predict(croppend_image)
+        # Capture frame-by-frame
+        key = cv2.waitKey(1)
+        if key & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 
-# load image
-#print("Hamburg")
-#image = read_image_bgr('Hamburg.jpg')
-#predict( image )
 
 # Build dictionary with class number and lable name labels_to_names = {0: 'person', 1: 'bicycle')
 labels_to_names = {}
@@ -116,18 +119,5 @@ with open('./images/classes.csv', mode='r') as csv_file:
         labels_to_names[int(row["class"])] = row["label"]
 
 print(labels_to_names)
+webcam( )
 
-print("-----------------------------------------------------------")
-print("1x4LRed")
-image = read_image_bgr('./images/frame20190125-125636.jpg')
-predict( image )
-
-print("-----------------------------------------------------------")
-print("1x5LBlack")
-image = read_image_bgr('./images/frame20190131-070052.jpg')
-predict( image )
-
-print("-----------------------------------------------------------")
-print("Multi")
-image = read_image_bgr('./images/frame20190131-071501.jpg')
-predict( image )
